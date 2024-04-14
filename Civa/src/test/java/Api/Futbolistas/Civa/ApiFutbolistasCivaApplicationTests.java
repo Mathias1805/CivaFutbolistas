0d868f54a7bd25360dc4ac91a7465d10d1580f44
Mathias1805 <120082878+Mathias1805@users.noreply.github.com>
2024-04-14 14:26:38 -0500

    Inicializando Proyecto

---
+++ Civa/src/test/java/Api/Futbolistas/Civa/ApiFutbolistasCivaApplicationTests.java
@@ -0,0 +1,13 @@
+package Api.Futbolistas.Civa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFutbolistasCivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
